fb40c604df43dad66447018eda8d7695bfc3765c
sharonamr <a.sharon@gmail.com>
2025-11-24 18:32:15 +0200

    Update utils.js

--- packages/affected/src/utils.js
+++ packages/affected/src/utils.js
@@ -1,8 +1,7 @@
 import { spawnSync } from 'child_process';
 
 export const getAffectedFiles = base => {
-	console.log('running git command:', `diff --name-only ${base}...HEAD`);
-	const ret = spawnSync('git', `diff --name-only ${base}...HEAD`.split(' '), {
+	const ret = spawnSync('git', `diff-tree --no-commit-id --name-only -r ${base} HEAD`.split(' '), {
     cwd: process.cwd(),
 		stdio: 'pipe',
     encoding: 'utf-8',
